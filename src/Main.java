3731bf13718e761637cdd1b7bd232319e797169a
Oleg Filippov <5157175@bk.ru>
2022-01-07 18:07:11 +0300

    Code reformat

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService BmiService = new BmiService();
-    float height = 1.4F;
-    float weight = 70.0F;
-    float index = BmiService.calculate(weight, height);
+        float height = 1.4F;
+        float weight = 70.0F;
+        float index = BmiService.calculate(weight, height);
         System.out.println(index + " Индекс Массы Тела");
     }
 }
